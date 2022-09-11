c34e64fe69b4ffd7e1b58f609b10eb7fb0dc191f
Bikash Chauhan <chauhanbikash789@gmail.com>
2022-09-11 14:58:39 +0530

    Added server setup files

---
+++ server/database/User.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = model;
+
+function model(sequelize) {
+  const attributes = {
+    firstName: { type: DataTypes.STRING, allowNull: false },
+    lastName: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false },
+  };
+  return sequelize.define("User", attributes);
+}
---
+++ server/database/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  HOST: "https://84.16.224.136",
+  USER: "lamshalp_test",
+  PASSWORD: "Test@2022",
+  DB: "lamshalp_test",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
